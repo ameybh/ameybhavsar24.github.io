b1dd1ba20413790c687ed130df91d9546b7542df
Amey Bhavsar <iamamey24@gmail.com>
2020-12-03 22:11:33 +0530

    removed semicolons from all js and jsx files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept()
 }
