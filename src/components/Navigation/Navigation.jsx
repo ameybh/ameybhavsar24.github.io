4ab9eafbcf5664e8b006f759d29b04d8e4e9bb59
Amey Bhavsar <iamamey24@gmail.com>
2020-12-04 13:23:59 +0530

    upgraded Navigation
    * Got scaleX working without overflow
    * Changed nav toggle to animation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,31 +1,38 @@
 import './Navigation.css'
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Row } from 'react-bootstrap';
 export default function Navigation() {
     return (
-        <Navbar expand="sm">
-            <Container fluid>
-                <Navbar.Brand href="/">
-                    AmeyBhavsar
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link>
-                            About
-                        </Nav.Link>
-                        <Nav.Link>
-                            Work
-                        </Nav.Link>
-                        <Nav.Link>
-                            Resume
-                        </Nav.Link>
-                        <Nav.Link>
-                            Contact
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div>
+            <Navbar expand="sm">
+                <Container fluid>
+                    <Navbar.Brand href="/">
+                        AmeyBhavsar
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
 
+                        <div class="bar1"></div>
+                        <div class="bar2"></div>
+                        <div class="bar3"></div>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link>
+                                About
+                        </Nav.Link>
+                            <Nav.Link>
+                                Work
+                        </Nav.Link>
+                            <Nav.Link>
+                                Resume
+                        </Nav.Link>
+                            <Nav.Link>
+                                Contact
+                        </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
+                </Container>
+            </Navbar>
+        </div>
     )
 }
