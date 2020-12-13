7a440814b88f1f214d7f41a1338e377ce7cd2466
Amey Bhavsar <iamamey24@gmail.com>
2020-12-13 23:31:05 +0530

    Added heading

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,9 @@ export default function Hero () {
     return (
         <Jumbotron fluid className="hero">
             <Container fluid>
-                <h1 className="hero__heading">Designer</h1>
-                <h1 className="hero__heading">+Web</h1>
-                <h1 className="hero__heading">Developer</h1>
+                <h1 className="hero__heading">I build</h1>
+                <h1 className="hero__heading">websites.</h1>
+                <h1 className="hero__heading animateDelay-1">For &nbsp;you.</h1>
             </Container>
         </Jumbotron>
     )
