ca0e7da4beaee7aed0c352d81ac3ec0250018abb
Amey Bhavsar <iamamey24@gmail.com>
2020-12-13 22:45:25 +0530

    temp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,9 +4,9 @@ export default function Hero () {
     return (
         <Jumbotron fluid className="hero">
             <Container fluid>
-                <h1>Designer</h1>
-                <h1>+Web</h1>
-                <h1>Developer</h1>
+                <h1 className="hero__heading">Designer</h1>
+                <h1 className="hero__heading">+Web</h1>
+                <h1 className="hero__heading">Developer</h1>
             </Container>
         </Jumbotron>
     )
