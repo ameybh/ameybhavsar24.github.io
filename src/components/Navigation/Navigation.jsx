4a27945868db7f17551fbaa2f32d117bddb7c762
Amey Bhavsar <28699912+ameybhavsar24@users.noreply.github.com>
2021-06-25 23:21:32 +0530

    close navbar on click

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import './Navigation.scss'
 import { Fragment, useState, useEffect, useRef } from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 export default function Navigation() {
+	const [expanded, setExpanded] = useState(false)
     const [navBackground, setNavBackground] = useState(false)
     const navRef = useRef()
     navRef.current = navBackground
@@ -19,18 +20,17 @@ export default function Navigation() {
     }, [])
     return (
         <Fragment>
-            <Navbar expand="lg" fixed="top" style={{ transition: '.3s ease', backgroundColor: navBackground ? '#fdfdfd' : '#fcfcfc', borderColor: navBackground ? 'rgba(0,0,0,.1)' : 'transparent' }}>
+            <Navbar expanded={expanded} expand="lg" fixed="top" style={{ transition: '.3s ease', backgroundColor: navBackground ? '#fdfdfd' : '#fcfcfc', borderColor: navBackground ? 'rgba(0,0,0,.1)' : 'transparent' }}>
                 <Container fluid>
                     <Navbar.Brand href="/" className="noselect">
                         AmeyBhavsar
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
-
+                    <Navbar.Toggle onClick={() => setExpanded(!expanded)}  aria-controls="basic-navbar-nav">
                         <div className="bar1"></div>
                         <div className="bar2"></div>
                         <div className="bar3"></div>
                     </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse id="basic-navbar-nav" onClick={() => setExpanded(false)}>
                         <Nav className="ml-auto noselect">
                         <Nav.Link href='#about'>
                             About
